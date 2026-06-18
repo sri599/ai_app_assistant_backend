e1cd44858aa39a4607438588a87256cd2a74ef21
DESKTOP-NQMAICR\Dell <srinathjaya97@gmail.com>
2026-06-18 18:30:05 +0530

    updated server.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require(
-  "./routes/authRoutes"
-);
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "AI Call Assistant API Running",
+  });
+});
 
 app.use("/api/auth", authRoutes);
 
