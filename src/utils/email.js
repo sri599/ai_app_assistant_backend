4f97a79ab98af640a66b05dd809ce8f4684aa40d
DESKTOP-NQMAICR\Dell <srinathjaya97@gmail.com>
2026-07-02 16:39:12 +0530

    fix email bug

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,15 +1,24 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
 const sendEmail = async (to, subject, html) => {
   try {
+    await transporter.verify();
+    console.log("SMTP Connected");
+
     await transporter.sendMail({
       from: `"Sharyx AI Assistant" <${process.env.EMAIL_USER}>`,
       to,
@@ -19,7 +28,7 @@ const sendEmail = async (to, subject, html) => {
 
     console.log("Email sent to", to);
   } catch (err) {
-    console.error("Email error:", err.message);
+    console.error(err);
   }
 };
 
